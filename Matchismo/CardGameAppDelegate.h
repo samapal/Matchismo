acf0430cfedd2b77d2317bc3ce69c8991c6c68f7
bencullenn <BenCullen@mini.home>
2013-12-26 17:07:22 -0500

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Ben Cullen on 12/26/13.
+//  Copyright (c) 2013 Ben Cullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Ben Cullen on 12/26/13.
+//  Copyright (c) 2013 Ben Cullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
